43c251178acb69fd0beaa832ea0faaeb0dfcf0ce
Noane Dan <Noane.Dan@outlook.com>
2017-05-17 20:19:09 +0300

    Add review model

--- BarFinder/lib/model/User.js
+++ BarFinder/lib/model/User.js
@@ -8,6 +8,7 @@ var userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
 });
 
 userSchema.pre('validate', function (next) {
