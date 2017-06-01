73bd6c1513ffa92d7876ce1073b7bf95a39265ef
Dani <dume.daniel@ymail.com>
2017-06-01 22:18:43 +0300

    hotfix: fixed typo, should solve delete problem on backend

--- BarFinder/public/javascript/notifications.js
+++ BarFinder/public/javascript/notifications.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
     $("li button.glyphicon-remove").on("click",function () {
         event.stopPropagation();
-        $.post("http://10.10.10.10:3000/notifications/delete", {notificationId: $(this).attr("value"), function(response){
+        $.post("http://10.10.10.10:3000/notifications/delete", {notificationId: $(this).attr("value")}, function(response){
                 $(this).parent().remove();
-        }});
+        });
      });
 });
