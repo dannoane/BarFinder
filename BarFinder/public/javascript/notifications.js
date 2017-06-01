efce3d8f72a82afda870fe217d54814d21655bd5
Dani <dume.daniel@ymail.com>
2017-06-01 13:43:00 +0300

    Added notifications page, TODO: test on delete with new validators

---
+++ BarFinder/public/javascript/notifications.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $("li button.glyphicon-remove").on("click",function () {
+        event.stopPropagation();
+        $.post("http://10.10.10.10:3000/notifications/delete", {notificationId: $(this).attr("value"), function(response){
+            if (response === 'SUCCESS'){
+                $(this).parent().remove();
+            }
+        }});
+     });
+});
--- BarFinder/routes/index.js
+++ BarFinder/routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
 var User = require('./../lib/model/User').User;
+var Notification = require('./../lib/model/Notification');
+
+const mongoose = require('./../lib/model/Notification').mongoose;
 
 // ensure user is loged in
 router.all('^(?!/login)', require('connect-ensure-login').ensureLoggedIn());
@@ -10,5 +14,28 @@ router.all('^(?!/login)', require('connect-ensure-login').ensureLoggedIn());
 router.get('/', function(req, res, next) {
   res.render('home', { user: req.user });
 });
+
+router.get('/notifications', function(req, res, next){
+  User.findOne({username: req.user.username})
+    .populate('notifications')
+      .exec(function(err, user){
+        res.render('notifications', {username: req.user.username, notifications: user.notifications});
+      });
+})
+
+router.post('/notifications/delete', function(req, res, next){
+  User.findOne({username: req.user.username}, function(err, user){
+    var index = user.notifications.indexOf(req.body.notificationId);
+    if (index > -1){
+      user.notifications.splice(index, 1);
+      user.save(function(err){
+        if (err)
+          res.send(err.message);
+        else
+          res.send("SUCCESS");
+      });
+    }
+  });
+})
 
 module.exports = router;
