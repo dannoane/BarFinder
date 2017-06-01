e75003b578c05064f7b3c1569ce7167d729da74c
Dani <dume.daniel@ymail.com>
2017-06-01 14:12:33 +0300

    Notification delete on backend works TODO: make delete on frontend(ran out of notifications)

--- BarFinder/public/javascript/notifications.js
+++ BarFinder/public/javascript/notifications.js
@@ -3,9 +3,7 @@ $(document).ready(function() {
     $("li button.glyphicon-remove").on("click",function () {
         event.stopPropagation();
         $.post("http://10.10.10.10:3000/notifications/delete", {notificationId: $(this).attr("value"), function(response){
-            if (response === 'SUCCESS'){
                 $(this).parent().remove();
-            }
         }});
      });
 });
