53db1ad78a97acec0a1c688fbaec2222d834b78f
Dani <dume.daniel@ymail.com>
2017-06-01 22:44:40 +0300

    Should work, needs testing

--- BarFinder/public/javascript/search.js
+++ BarFinder/public/javascript/search.js
@@ -107,9 +107,33 @@ $(document).ready(function () {
     });
   
     var preferences = getCustomPreferences();
-
-    console.log(preferences);
-
+    $.post("http://10.10.10.10:3000/search/locations",{
+      attires = preferences.attires,
+      categories = preferences.categories,
+      foodStyles = preferences.foodStyles,
+      paymentOptions = preferences.paymentOptions,
+      restaurantServices = preferences.restaurantServices,
+      restaurantSpecialties = preferences.restaurantSpecialties,
+      priceRange = preferences.priceRange,
+      latitude = preferences.latitude,
+      longitude = preferences.longitude,
+      radius = preferences.radius
+    },
+      function(data){    
+        data.locations.forEach(function(location) { //TODO: how does the server send the info? only this row needs change
+          $(".locations.list-group").append("<div class=\"list-group-item short-location\"><div class=\"row location\"></div><div class=\"row long-description\"></div></div>");
+          var locationRow = $(".list-group-item.short-location > .row.location");
+          locationRow.append("<h4 class=\"col-sm-3 location-name\">"+location.name+"</h4>");
+          locationRow.append("<span class=\"col-sm-1 rating\">"+location.rating+"</span>");
+          locationRow.append("<span class=\"col-sm-2 address\">"+location.street+"</span>");
+          locationRow.append("<span class=\"col-sm-offset-1 col-sm-2 description\"><a href=\"#\">See More<i class=\"fa fa-caret-down\" aria-hidden=\"true\"></i></a></span><span class=\"col-sm-offset-1 col-sm-2 goto-rating\"><a href=\"#\">Give a rating</a></span>");
+          var locationDetails = $(".list-group-item.short-location > .row.long-description");
+          locationDetails.append("<span class=\"col-sm-12\">About: "+location.about+"</span>");
+          locationDetails.append("<span class=\"col-sm-12\">Phone: "+location.phone+"</span>");
+          //TODO: decide what to show and show it
+        }, this);
+      },"json"
+    );
   })
 
 });
